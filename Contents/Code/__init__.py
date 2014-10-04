4a60020a3cdf86721d17e09bb7d61a695733d231
sander1 <sander@plexapp.com>
2014-10-04 15:41:45 +0200

    Updated icon

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,5 @@
 NAME = 'Lifetime'
-ICON = 'icon-default.png'
+ICON = 'icon-default.jpg'
 ART = 'art-default.jpg'
 LT_BASE = '/video/lifetime'
 
@@ -12,6 +12,7 @@ RE_JSON  = Regex('jQuery.extend\(Drupal.settings, (.+?)}}\);')
 
 RE_SEASON  = Regex('Season (\d{1,3})')
 RE_EP  = Regex('Episode (\d{1,3})')
+
 ####################################################################################################
 def Start():
 
@@ -21,7 +22,7 @@ def Start():
     DirectoryObject.art = R(ART)
 
     HTTP.CacheTime = CACHE_1HOUR
-    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0'
+    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'
 
 ####################################################################################################
 @handler(LT_BASE, NAME, art=ART, thumb=ICON)
@@ -32,28 +33,28 @@ def MainMenu():
     oc.add(DirectoryObject(key=Callback(Shows, title="LMN Shows", show_type='LMN'), title="LMN Shows"))
     oc.add(DirectoryObject(key=Callback(Movies, title="Movies"), title="Movies"))
     return oc
-    
+
 ####################################################################################################
 # This function produces a list of TV shows from the lifetime show page
 @route(LT_BASE + '/shows')
 def Shows(title, show_type):
-    
+
     oc = ObjectContainer(title2=title)
     html = HTML.ElementFromURL(LT_SHOWS)
-    
+
     for shows in html.xpath('//*[text()="%s"]/following::ul[contains(@class,"menu list-3")][1]//li/a' %show_type):
         show = shows.xpath('.//text()')[0]
         url = shows.xpath('./@href')[0]
 
         oc.add(DirectoryObject(key = Callback(Sections, title=show, url=url), title=show))
-            
+
     return oc
-    
+
 ####################################################################################################
 # This function checks a show for a video and/or full episode page
 @route(LT_BASE + '/sections')
 def Sections(title, url):
-    
+
     oc = ObjectContainer(title2=title)
     html = HTML.ElementFromURL(url)
     # Check for a video and/or full episode link in navigation bar
@@ -69,20 +70,20 @@ def Sections(title, url):
         return ObjectContainer(header="Empty", message="There are no videos for this show.")
     else:
         return oc
-            
+
 ####################################################################################################
 # This function uses the json in each page to process videos on that page
 # This can process the new style of videos
 @route(LT_BASE + '/videos')
 def Videos(title, url=''):
-    
+
     oc = ObjectContainer(title2=title)
     content = HTTP.Request(url).content
     json_data = RE_JSON.search(content).group(1)
     json_data = json_data + '}}'
     #Log('the value of json_data is %s' %json_data)
     json = JSON.ObjectFromString(json_data)
-    
+
     for video in json['video']['videos']:
         vid_title = video['title']
         try: unlocked = video['video_behind_wall']
@@ -130,7 +131,6 @@ def Videos(title, url=''):
                 originally_available_at = vid_date,
                 thumb = Resource.ContentsOfURLWithFallback(url=thumb)
             ))
-            
 
     try:
         html = HTML.ElementFromString(content)
@@ -146,16 +146,16 @@ def Videos(title, url=''):
         return ObjectContainer(header="Empty", message="There are no unlocked full episodes listed for this show.")
     else:
         return oc
-    
+
 ####################################################################################################
 # This function produces a list of movies from the lifetime movies watch online page
 # The json pull does not work on this page
 @route(LT_BASE + '/movies')
 def Movies(title):
-    
+
     oc = ObjectContainer(title2=title)
     html = HTML.ElementFromURL(LT_MOVIES)
-    
+
     for movies in html.xpath('//div[@class="movie-details"]'):
         premium_list = movies.xpath('./a/div/@class')
         if 'premium-new' in premium_list:
@@ -170,6 +170,5 @@ def Movies(title):
             title = title,
             thumb = Resource.ContentsOfURLWithFallback(url=thumb)
         ))
-            
+
     return oc
-    
